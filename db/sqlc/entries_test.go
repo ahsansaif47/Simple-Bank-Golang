9c87979908f817d0e146146fb4200310c8c7fd2a
ahsansaif47 <ahsan_saif47@hotmail.com>
2023-01-11 21:59:10 +0500

    entry testing

---
+++ db/sqlc/entries_test.go
@@ -0,0 +1,14 @@
+package db
+
+import "testing"
+
+func CreateRandomEntry(t *testing.T) {
+	entry := CreateEntryParams{
+		AccountID: utils.,
+		Amount:    0,
+		CreatedAt: time.Time{},
+	}
+}
+func TestCreateEntry(t *testing.T) {
+
+}
