ba10584a6f250b3a36039e98279699a25ceb146c
ahsansaif47 <ahsan_saif47@hotmail.com>
2023-01-11 21:58:48 +0500

    account testing complete

--- db/query/account.sql
+++ db/query/account.sql
@@ -16,10 +16,12 @@ SELECT *
 FROM accounts
 ORDER BY id
 LIMIT $1 OFFSET $2;
--- name: UpdateAccount :exec
+-- name: UpdateAccount :one
 UPDATE accounts
 SET balance = $2
-WHERE id = $1;
--- name: DeleteAccount :exec
+WHERE id = $1
+Returning *;
+-- name: DeleteAccount :one
 DELETE FROM accounts
-WHERE id = $1;
+WHERE id = $1
+Returning *;
--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func CreateRandomAccount(t *testing.T) Account {
 	args := CreateAccountParams{
 		Owner:    utils.RandomOwner(),
 		Balance:  utils.RandomMoney(),
@@ -29,4 +29,78 @@ func TestCreateAccount(t *testing.T) {
 	// check if auto-fill columns are filled..
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	CreateRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	// check if there is no error..
+	require.NoError(t, err)
+	// check if the account object is not empty..
+	require.NotEmpty(t, account2)
+
+	// checking the fields of created account vs the account queried..
+	require.Equal(t, account2.ID, account1.ID)
+	require.Equal(t, account2.Owner, account1.Owner)
+	require.Equal(t, account2.Currency, account1.Currency)
+	require.Equal(t, account2.Balance, account1.Balance)
+	require.Equal(t, account2.CreatedAt, account1.CreatedAt)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := CreateRandomAccount(t)
+
+	args := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: utils.RandomMoney(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), args)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account2.Balance, args.Balance)
+	require.Equal(t, account2.CreatedAt, account.CreatedAt)
+	require.Equal(t, account2.Currency, account.Currency)
+	require.Equal(t, account2.ID, account.ID)
+	require.Equal(t, account2.Owner, account.Owner)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := CreateRandomAccount(t)
+
+	del_account, err := testQueries.DeleteAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), del_account.ID)
+	require.Error(t, err)
+	require.Empty(t, account2)
+	require.Equal(t, del_account.ID, account.ID)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		CreateRandomAccount(t)
+	}
+
+	args := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), args)
+	require.NoError(t, err)
+
+	// checking if the accounts are not-empty
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+
 }
