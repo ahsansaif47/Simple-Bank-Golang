73495a107e5a5e47fe105a6d13c92c83e56feeee
ahsansaif47 <ahsan_saif47@hotmail.com>
2023-01-27 23:40:52 +0500

    code refactored

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -97,32 +97,12 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		// Update account balance
 		// Updating account balance in an order(to avoid deadlocks)..
 		if arg.FromAccountID < arg.ToAccountID {
-			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.FromAccountID,
-				Amount: -arg.Amount,
-			})
-			if err != nil {
-				return err
-			}
-			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.ToAccountID,
-				Amount: arg.Amount,
-			})
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
 			if err != nil {
 				return err
 			}
 		} else {
-			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.ToAccountID,
-				Amount: arg.Amount,
-			})
-			if err != nil {
-				return err
-			}
-			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				ID:     arg.FromAccountID,
-				Amount: -arg.Amount,
-			})
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 			if err != nil {
 				return err
 			}
@@ -133,4 +113,26 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		return result, err
 	}
 	return result, nil
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	fromAccountID int64,
+	amount1 int64,
+	toAccountID int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		Amount: amount1,
+		ID:     fromAccountID,
+	})
+	if err != nil {
+		return
+	}
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		Amount: amount2,
+		ID:     toAccountID,
+	})
+	return
 }
